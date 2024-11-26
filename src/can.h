01568637d54273c60e5ee65251d42d66f40b23bc
Yasushi SHOJI <yashi@spacecubics.com>
2024-11-26 20:35:05 +0900

    can: Introduce CAN_MAX_DLC
    This commit replace literal 8 with CAN_MAX_DLC.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/can.h
+++ src/can.h
@@ -7,9 +7,11 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define CAN_MAX_DLC 8U
+
 typedef union canbuf {
         uint32_t val[2];
-        uint8_t data[8];
+        uint8_t data[CAN_MAX_DLC];
 } canbuf_t;
 
 void can_init(void);
